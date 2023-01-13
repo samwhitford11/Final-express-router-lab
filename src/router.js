8d312f344b1d37ee149636a763345acc5f2dcd40
samwhitford11 <samantha.fichte98@gmail.com>
2023-01-12 17:36:28 -0700

    fix error in router

--- src/router.js
+++ src/router.js
@@ -8,8 +8,8 @@ import { aboutLoader, projectsLoader } from "./loaders";
 const router = createBrowserRouter(createRoutesFromElements(
     <Route path="/" element={<App/>}>
          <Route path="" element={<Home/>}/>
-        <Route path="/about" element={<About/>} loader={aboutLoader}/>
-        <Route path="/projects" element={<Projects/>} loader={projectsLoader}/>
+        <Route path="about" element={<About/>} loader={aboutLoader}/>
+        <Route path="projects" element={<Projects/>} loader={projectsLoader}/>
 
     </Route>
 ))
