8dae2d085f4f7a8f47e09f839e469ec10bbba754
samwhitford11 <samantha.fichte98@gmail.com>
2023-01-12 20:27:23 -0700

    links css file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Outlet } from "react-router-dom";
+import "./App.css";
 
 
 function App() {
-  const URL = "http://localhost:4111/";
   return (
     <div className="App">
       <Header/>
