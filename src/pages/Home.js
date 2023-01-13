408f0187fc898246c726f98cc12712e57c111d92
samwhitford11 <samantha.fichte98@gmail.com>
2023-01-12 21:16:19 -0700

    more info on home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,10 @@
 function Home(props) {
-    return <h1>Home</h1>;
+    return (
+    <h1>Hi, I'm Samantha</h1>,
+    <h2>At heart I've always been artistic and had a passion for making all my work aesthetically pleasing. Entering the ditigal world of software engineering, I'm excited to create apps that will make users coming back for more.  </h2>
+
+
+    )
   }
   
   export default Home;
